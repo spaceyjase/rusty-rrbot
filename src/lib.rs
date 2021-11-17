5e0047a0c338e663f9b03247240f6cbb32bca385
jason <429978+spacyerjase@users.noreply.github.com>
2021-11-17 14:54:43 +0000

    Read post titles and comments.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 extern crate orca;
-use orca::App;
+use orca::{App, Sort};
 use serde::Deserialize;
 use serde_json::Result;
 use std::fs;
@@ -24,6 +24,21 @@ impl Config {
   }
 }
 
+#[derive(Deserialize)]
+pub struct Post {
+  pub id: String,
+  pub title: String,
+  pub selftext: String,
+}
+
+impl Post {
+  pub fn new(json: &str) -> Result<Post> {
+    let post: Post = serde_json::from_str(&json).expect("Error parsing json");
+
+    Ok(post)
+  }
+}
+
 lazy_static! {
   static ref RE: Regex = {
     let re = Regex::new(r#"^(?i)rr\?$|^(w[h]?at|wtf)('s| is| does)? (a[n]? rr|the rr|rr)( mean| stand for| and where do i find it)?[\?\.]?$|(?<!")(?<! is |did )(w[h]?at|wtf)('s| is| does)? (a[n]? rr|the rr|rr)( mean| stand for| and where do i find it)?(?!outine| \w)[\?\.]?(?!")|define rr[\?\.]?"#).unwrap();
@@ -37,16 +52,26 @@ lazy_static! {
   };
 }
 
-pub fn run() -> Result<()> {
+fn init_reddit() -> App {
   let contents = fs::read_to_string("config.json").expect("Error reading config file");
-
   let config = Config::new(&contents).unwrap();
-
   let mut reddit = App::new("Linux:com.jasonmichaeladams.rrbot", "0.1", "u/spaceyjase").unwrap();
   reddit.authorize_script(&config.client_id, &config.client_secret, &config.username, &config.password).unwrap();
+  reddit
+}
 
-  let user =  reddit.get_self().unwrap();
-  println!("Got data: {}", user);
+pub fn run() -> Result<()> {
+  let reddit = init_reddit();
+
+  let posts = reddit.get_posts("bodyweightfitness", Sort::Hot).unwrap();
+  for json in posts["data"]["children"].as_array().unwrap() {
+    let post = Post::new(&json["data"].to_string()).unwrap();
+    println!("{}:{}", post.id, post.title);
+    let comments = reddit.get_comment_tree(&post.id).unwrap();
+    for comment in comments {
+      println!("\t- {}", comment.body);
+    }
+  }
 
   Ok(())
 }
@@ -72,6 +97,119 @@ mod tests {
     assert_eq!(config.client_secret, "client_secret");
     assert_eq!(config.username, "username");
     assert_eq!(config.password, "password");
+  }
+
+  #[test]
+  fn test_post_parse() {
+    let json = r#"
+      {
+        "all_awardings": [],
+        "allow_live_comments": false,
+        "approved_at_utc": null,
+        "approved_by": null,
+        "archived": false,
+        "author": "AutoModerator",
+        "author_flair_background_color": null,
+        "author_flair_css_class": null,
+        "author_flair_richtext": [],
+        "author_flair_template_id": null,
+        "author_flair_text": null,
+        "author_flair_text_color": null,
+        "author_flair_type": "text",
+        "author_fullname": "t2_6l4z3",
+        "author_is_blocked": false,
+        "author_patreon_flair": false,
+        "author_premium": true,
+        "awarders": [],
+        "banned_at_utc": null,
+        "banned_by": null,
+        "can_gild": false,
+        "can_mod_post": false,
+        "category": null,
+        "clicked": false,
+        "content_categories": null,
+        "contest_mode": false,
+        "created": 1637150412.0,
+        "created_utc": 1637150412.0,
+        "discussion_type": null,
+        "distinguished": null,
+        "domain": "self.bodyweightfitness",
+        "downs": 0,
+        "edited": false,
+        "gilded": 0,
+        "gildings": {},
+        "hidden": false,
+        "hide_score": false,
+        "id": "qvxrbp",
+        "is_created_from_ads_ui": false,
+        "is_crosspostable": false,
+        "is_meta": false,
+        "is_original_content": false,
+        "is_reddit_media_domain": false,
+        "is_robot_indexable": true,
+        "is_self": true,
+        "is_video": false,
+        "likes": null,
+        "link_flair_background_color": "",
+        "link_flair_css_class": null,
+        "link_flair_richtext": [],
+        "link_flair_text": null,
+        "link_flair_text_color": "dark",
+        "link_flair_type": "text",
+        "locked": false,
+        "media": null,
+        "media_embed": {},
+        "media_only": false,
+        "mod_note": null,
+        "mod_reason_by": null,
+        "mod_reason_title": null,
+        "mod_reports": [],
+        "name": "t3_qvxrbp",
+        "no_follow": false,
+        "num_comments": 7,
+        "num_crossposts": 0,
+        "num_reports": null,
+        "over_18": false,
+        "parent_whitelist_status": "all_ads",
+        "permalink": "/r/bodyweightfitness/comments/qvxrbp/bwf_daily_discussion_and_beginnerrr_questions/",
+        "pinned": false,
+        "pwls": 6,
+        "quarantine": false,
+        "removal_reason": null,
+        "removed_by": null,
+        "removed_by_category": null,
+        "report_reasons": null,
+        "saved": false,
+        "score": 1,
+        "secure_media": null,
+        "secure_media_embed": {},
+        "selftext": " Welcome to the [r/bodyweightfitness](https://www.reddit.com/r/bodyweightfitness/) daily discussion thread!\n\nFeel free to post beginner questions or just about anything that's on your mind related to fitness!\n\n**Reminders:**\n\n* Read the [FAQ](http://www.reddit.com/r/bodyweightfitness/wiki/faq) as your question may be answered there already.\n* If you're unsure how to start training, try the [BWF Primer Routine](https://www.reddit.com/r/bodyweightfitness/comments/kofo8l/bwf_primer_buildup_community_event_day_1_happy/), check out our [Recommended Routine](https://www.reddit.com/r/bodyweightfitness/wiki/kb/recommended_routine), or our more skills based routine: [Move](https://www.reddit.com/r/bodyweightfitness/wiki/move).\n* Even though the rules are relaxed here, asking for medical advice is still not allowed.\n\nJoin our live conversations on [Discord](https://discord.gg/bwf)! We're also on [Facebook](https://www.facebook.com/redditbwf), [Instagram](https://www.instagram.com/redditbwf), and [Twitter](https://www.twitter.com/redditbwf)!\n\nIf you'd like to look at previous Discussion threads, [click here.](https://www.reddit.com/r/bodyweightfitness/search?q=Daily+Discussion+and+Beginner&amp;sort=new&amp;restrict_sr=on)",
+        "selftext_html": "&lt;!-- SC_OFF --&gt;&lt;div class=\"md\"&gt;&lt;p&gt;Welcome to the &lt;a href=\"https://www.reddit.com/r/bodyweightfitness/\"&gt;r/bodyweightfitness&lt;/a&gt; daily discussion thread!&lt;/p&gt;\n\n&lt;p&gt;Feel free to post beginner questions or just about anything that&amp;#39;s on your mind related to fitness!&lt;/p&gt;\n\n&lt;p&gt;&lt;strong&gt;Reminders:&lt;/strong&gt;&lt;/p&gt;\n\n&lt;ul&gt;\n&lt;li&gt;Read the &lt;a href=\"http://www.reddit.com/r/bodyweightfitness/wiki/faq\"&gt;FAQ&lt;/a&gt; as your question may be answered there already.&lt;/li&gt;\n&lt;li&gt;If you&amp;#39;re unsure how to start training, try the &lt;a href=\"https://www.reddit.com/r/bodyweightfitness/comments/kofo8l/bwf_primer_buildup_community_event_day_1_happy/\"&gt;BWF Primer Routine&lt;/a&gt;, check out our &lt;a href=\"https://www.reddit.com/r/bodyweightfitness/wiki/kb/recommended_routine\"&gt;Recommended Routine&lt;/a&gt;, or our more skills based routine: &lt;a href=\"https://www.reddit.com/r/bodyweightfitness/wiki/move\"&gt;Move&lt;/a&gt;.&lt;/li&gt;\n&lt;li&gt;Even though the rules are relaxed here, asking for medical advice is still not allowed.&lt;/li&gt;\n&lt;/ul&gt;\n\n&lt;p&gt;Join our live conversations on &lt;a href=\"https://discord.gg/bwf\"&gt;Discord&lt;/a&gt;! We&amp;#39;re also on &lt;a href=\"https://www.facebook.com/redditbwf\"&gt;Facebook&lt;/a&gt;, &lt;a href=\"https://www.instagram.com/redditbwf\"&gt;Instagram&lt;/a&gt;, and &lt;a href=\"https://www.twitter.com/redditbwf\"&gt;Twitter&lt;/a&gt;!&lt;/p&gt;\n\n&lt;p&gt;If you&amp;#39;d like to look at previous Discussion threads, &lt;a href=\"https://www.reddit.com/r/bodyweightfitness/search?q=Daily+Discussion+and+Beginner&amp;amp;sort=new&amp;amp;restrict_sr=on\"&gt;click here.&lt;/a&gt;&lt;/p&gt;\n&lt;/div&gt;&lt;!-- SC_ON --&gt;",
+        "send_replies": true,
+        "spoiler": false,
+        "stickied": true,
+        "subreddit": "bodyweightfitness",
+        "subreddit_id": "t5_2tf0a",
+        "subreddit_name_prefixed": "r/bodyweightfitness",
+        "subreddit_subscribers": 2289660,
+        "subreddit_type": "public",
+        "suggested_sort": "new",
+        "thumbnail": "",
+        "title": "BWF Daily Discussion and Beginner/RR Questions Thread for 2021-11-17",
+        "top_awarded_type": null,
+        "total_awards_received": 0,
+        "treatment_tags": [],
+        "ups": 1,
+        "upvote_ratio": 1.0,
+        "url": "https://www.reddit.com/r/bodyweightfitness/comments/qvxrbp/bwf_daily_discussion_and_beginnerrr_questions/",
+        "user_reports": [],
+        "view_count": null,
+        "visited": false,
+        "whitelist_status": "all_ads",
+        "wls": 6
+      }"#;
+    let post = Post::new(json).unwrap();
+    assert_eq!(post.id, "qvxrbp");
   }
 
   #[test]
